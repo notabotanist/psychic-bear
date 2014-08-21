37583ed08cb669ac6e7e3479ef3b4f9dfd5ae79b
Matt MacEwan(Netbook) <mrm4677@cs.rit.edu>
2014-08-20 23:10:34 -0400

    Build Graph collection with empty bars and a vote count

--- Handler/Dealer.hs
+++ Handler/Dealer.hs
@@ -3,9 +3,54 @@ module Handler.Dealer where
 import Import
 import Safe (maximumDef)
 import Data.Time (getCurrentTime)
+import Data.List (groupBy)
+
+import Data.Function (on)
+import Model.ScrumBet
 
 getHandList =
   runDB $ selectList [] [ Desc HandCreatedDate, LimitTo 5 ]
+
+getBidList handId =
+  runDB $ selectList [ ScrumBetHand ==. handId ] [ Asc ScrumBetLastUpdated ]
+
+compareBetEntitiesByVal :: Entity ScrumBet -> Entity ScrumBet -> Bool
+compareBetEntitiesByVal = (==) `on` scrumBetValue . entityVal
+
+data BetGraphBar = BetGraphBar
+  { label :: FibonacciSubset
+  , count :: Int
+  }
+  deriving Show
+
+-- Perhaps also include total votes for bar scaling
+data BetGraph = BetGraph
+  { numVotes :: Int
+  , bars :: [BetGraphBar]
+  }
+
+--mkBetGraphBar :: FibonacciSubset -> [FibonacciSubset] -> BetGraphBar
+--mkBetGraphBar def []        = BetGraphBar def 0
+--mkBetGraphBar _   l@(fib:_) = BetGraphBar fib (length l)
+
+--mkBetGraph :: [[FibonacciSubset]] -> BetGraph
+--mkBetGraph [] = BetGraph 0 []
+--mkBetGraph betGroups = BetGraph fillDomain (head.head betGroups) betGroups
+
+mkBetGraph :: [FibonacciSubset] -> BetGraph
+mkBetGraph [] = BetGraph 0 []
+mkBetGraph flatVotes@(f:_) = BetGraph (length flatVotes) (foldBars f flatVotes)
+  where
+    foldBars _ [] = []
+    foldBars targetF votesTail =
+        let (targetVotes, remainingVotes) = span (== targetF) votesTail in
+        (BetGraphBar targetF (length targetVotes)) : foldBars (bSucc targetF) remainingVotes
+    -- bounded successor
+    bSucc F55 = F55
+    bSucc x   = succ x
+    
+  
+-- End pure util functions.  Move to shared module later
 
 handForm :: Form Hand
 handForm = renderDivs $ Hand
@@ -30,4 +75,7 @@ postDealerR = do
     _ -> getDealerR
 
 getDealerViewR :: HandId -> Handler Html
-getDealerViewR = undefined
+getDealerViewR handId = do
+  betEntities <- getBidList handId
+  let groupedBets = groupBy compareBetEntitiesByVal betEntities
+  undefined
