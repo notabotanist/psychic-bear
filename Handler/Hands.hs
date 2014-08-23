fee77766471323845c8f841dff283b69f190be7f
Matt MacEwan(Netbook) <mrm4677@cs.rit.edu>
2014-08-23 01:32:05 -0400

    Basic bet choice form
    Eventually, authentication will play a role in this form.

--- Handler/Hands.hs
+++ Handler/Hands.hs
@@ -1,6 +1,22 @@
 module Handler.Hands where
 
 import Import
+import Control.Arrow ((&&&))
+import Data.Time (getCurrentTime)
+
+import Model.ScrumBet
+
+userIdSessionKey :: Text
+userIdSessionKey = "scrumuserid"
+
+scrumBetForm :: HandId -> Int -> Form ScrumBet
+scrumBetForm handId userId = renderDivs $ ScrumBet
+  <$> pure handId
+  <*> pure userId
+  <*> areq (selectFieldList fibs) "Value" (Just F0)
+  <*> lift (liftIO getCurrentTime)
+  where
+    fibs = map (toText &&& id) [(F0)..]
 
 getHandsR :: Handler Html
 getHandsR = do
@@ -10,7 +26,13 @@ getHandsR = do
     $(widgetFile "handlist")
 
 getBettorViewR :: HandId -> Handler Html
-getBettorViewR = undefined
+getBettorViewR handId = do
+  (formWidget, _) <- generateFormPost $ scrumBetForm handId 0
+  defaultLayout $ [whamlet|
+    <form method=post action=@{BetsR handId 0}>
+      ^{formWidget}
+      <input type=submit value="Place your bet">
+    |]
 
 postBetsR :: HandId -> Int -> Handler Value
 postBetsR = undefined
