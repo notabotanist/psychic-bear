8cc50e127f276b2839d5830ed8949c14a9b7c499
Matt MacEwan(Netbook) <mrm4677@cs.rit.edu>
2014-09-07 17:00:15 -0400

    Added route to handle showdown timing
    The showdown endpoint is designed for AJAX calls to signal the start of
    a round of voting and to query the consensus when the round is over.

---
+++ Handler/Showdown.hs
@@ -0,0 +1,9 @@
+module Handler.Showdown where
+
+import Import
+
+getShowdownR :: HandId -> Handler Value
+getShowdownR handId = undefined
+
+postShowdownR :: HandId -> Handler Value
+postShowdownR handId = undefined
